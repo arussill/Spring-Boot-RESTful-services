5a9d7252f670c672576aa8eb1ccb06fd6853d3f1
Aura Russill <auracris1996@hotmail.com>
2022-06-01 09:24:02 -0500

    Inicio del proyecto
    Ejercico de practica de con Spring Boot RESTful services

---
+++ src/main/java/com/sofkau/springbootrestfulservices/SpringBootResTfulServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.springbootrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootResTfulServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/springbootrestfulservices/SpringBootResTfulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.springbootrestfulservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
