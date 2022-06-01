3dcd0462683054746afdfb4717bc90e0bc0bc7f7
Aura Russill <auracris1996@hotmail.com>
2022-06-01 09:44:00 -0500

    ADD
    Se agrego codigo a las diferentes clases en los diferentes paquetes creados anteriormente, controlador, empleadodto, empleadomapper, empleado, repositorioempleado y servicioempleado

--- src/main/java/com/sofkau/springbootrestfulservices/repository/RepositorioEmpleado.java
+++ src/main/java/com/sofkau/springbootrestfulservices/repository/RepositorioEmpleado.java
@@ -1,4 +1,7 @@
 package com.sofkau.springbootrestfulservices.repository;
 
-public class RepositorioEmpleado {
+import com.sofkau.springbootrestfulservices.models.Empleado;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositorioEmpleado extends MongoRepository<Empleado, String> {
 }
